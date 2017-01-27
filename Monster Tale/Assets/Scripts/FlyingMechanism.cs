d1ed861123b99cd4b5f048072384aa409912b1c5
jandzouana <jandzouana@gmail.com>
2017-01-27 01:47:02 -0800

    startng spaceship mechanisms

---
+++ Monster Tale/Assets/Scripts/FlyingMechanism.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingMechanism : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GvrController.IsTouching)
+        {
+            Vector2 touchPos = 2 * GvrController.TouchPos - Vector2.one;
+             //Debug.Log(touchPos);
+            
+            if(touchPos.x >= touchPos.y && touchPos.y > -touchPos.x)
+            {
+                Debug.Log("Right");
+            }
+            else if (touchPos.x <= touchPos.y && touchPos.y < -touchPos.x)
+            {
+                Debug.Log("Left");
+            }
+
+            else if (touchPos.y >= -touchPos.x && touchPos.y > touchPos.x)
+            {
+                Debug.Log("Bottom");
+            }
+
+            else if (touchPos.y <= -touchPos.x && touchPos.y < touchPos.x)
+            {
+                Debug.Log("Top");
+            }
+
+        }
+    }
+}

dba639a04816eb18aeb9f5c9ba8028b29ab8c6fc
jandzouana <jandzouana@gmail.com>
2017-01-30 23:36:03 -0800

    finished implementing flying mechanisms.
    -rotated player forward when seated in spaceship
    -fixed top spaceship rotation problem

--- Monster Tale/Assets/Scripts/FlyingMechanism.cs
+++ Monster Tale/Assets/Scripts/FlyingMechanism.cs
@@ -4,69 +4,101 @@ using System.Collections;
 public class FlyingMechanism : MonoBehaviour {
     public Transform spaceship;
     public float speed;
-    public float turnSpeed = 20f;
+    public float turnSpeed;
     public bool moveForward;
+    public bool moveBack;
     public GameObject player; // where script for sitInSpaceship is located
     public bool hasBeenDelayed;
 
-    private float defspeed = 0.50f;
-	private sitInSpaceship sittingScript; // function if is sitting
+    private float defSpeed = 0.50f;
+    private float defTurnSpeed = 30f;
+
+    private sitInSpaceship sittingScript; // function if is sitting
 	private CharacterController cc;
     private CharacterController pcc;
     private bool isCoroutineExecuting = false;
     private float time = 3f;
+    public GameObject target;
+
+
+    public void DeactivateTarget()
+    {
+        target.GetComponent<spaceship_open>().enabled = false;
+    }
 
     // Use this for initialization
     void Start()
     {
         sittingScript = player.GetComponent<sitInSpaceship>();
         cc = GetComponent<CharacterController>();
-        speed = defspeed;
+        speed = defSpeed;
+        turnSpeed = defTurnSpeed;
         moveForward = false;
+        moveBack = false;
         pcc = player.GetComponent<CharacterController>();
         hasBeenDelayed = false;
     }
 
     // Update is called once per frame
     void Update () {
+        //Moving forward
         if (moveForward)
         {
             Vector3 forward = spaceship.TransformDirection(Vector3.forward); // getting forward direction
             cc.Move(forward * speed);
-            pcc.Move(forward * speed);
 		}
+        //Moving backward
+        if (moveBack)
+        {
+            Vector3 forward = spaceship.TransformDirection(Vector3.forward); // getting forward direction
+            cc.Move(-forward * speed);
+        }
+        //Forward
         if(GvrController.ClickButtonDown && sittingScript.isInsideSpaceship && !moveForward &&hasBeenDelayed) 
         {
             moveForward = true;
+            moveBack = false;
         }
         else if (GvrController.ClickButtonDown && sittingScript.isInsideSpaceship && moveForward &&hasBeenDelayed)
         {
             moveForward = false;
         }
-        if (GvrController.IsTouching)
+        //Reverse
+        if (GvrController.AppButtonDown && sittingScript.isInsideSpaceship && !moveBack && hasBeenDelayed)
         {
+            moveForward = false;
+            moveBack = true;
+        }
+        else if (GvrController.AppButtonDown && sittingScript.isInsideSpaceship && moveBack && hasBeenDelayed)
+        {
+            moveBack = false;
+        }
+        if (GvrController.IsTouching && sittingScript.isInsideSpaceship)
+        {
+            DeactivateTarget();
             Vector2 touchPos = 2 * GvrController.TouchPos - Vector2.one;
-             //Debug.Log(touchPos);
 
             if(touchPos.x >= touchPos.y && touchPos.y > -touchPos.x)
             {
                 Debug.Log("Right");
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
             }
             else if (touchPos.x <= touchPos.y && touchPos.y < -touchPos.x)
             {
                 Debug.Log("Left");
                 transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-                player.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
             }
 
             else if (touchPos.y >= -touchPos.x && touchPos.y > touchPos.x)
             {
                 Debug.Log("Bottom");
+                transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
             }
 
             else if (touchPos.y <= -touchPos.x && touchPos.y < touchPos.x)
             {
                 Debug.Log("Top");
+                transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
             }
 
         }
--- Monster Tale/Assets/Scripts/sitInSpaceship.cs
+++ Monster Tale/Assets/Scripts/sitInSpaceship.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class sitInSpaceship : MonoBehaviour {
 	private Vector3 spaceshipPosition = new Vector3(68f, 3.94f, -0.19f); //location above spaceship seat
 	public bool isInsideSpaceship;
+    public GameObject spaceship;
 
 	public void TeleportToSpaceship(){
-		transform.localPosition = spaceshipPosition;
+        Vector3 to = new Vector3(0, 0, 0);
+        transform.localPosition = spaceshipPosition; //teleports player to spaceship location
 		isInsideSpaceship = true;
+        transform.SetParent(spaceship.transform); //attaches player to spaceship
+        transform.eulerAngles = to; //rotates the player to face forward
 	}
 	// Use this for initialization
 	void Start () {

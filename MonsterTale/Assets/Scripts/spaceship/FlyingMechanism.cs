c30c9c88e0f9e7065473b5878bd65749d5550776
Jessica Andzouana <jandzouana@gmail.com>
2017-06-06 22:30:32 -0700

    added flying mechanisms

---
+++ MonsterTale/Assets/Scripts/spaceship/FlyingMechanism.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingMechanism : MonoBehaviour {
+    public Transform spaceship;
+    public float speed; //changed SPEED
+    public float reverseSpeed = 0.25f;
+    public float rotationSpeed;
+    public float defSpeed = 0.50f;
+    private float defTurnSpeed = 30f;
+
+    public GameObject player; // where script for sitInSpaceship is located
+    public float holdTime = 1.0f;
+    public GameObject safeZone;
+    public GameObject controller;
+    public bool hitSafeZone;
+    public GameObject target;
+    public GameObject SittingScriptObject;
+
+    private Vector3 euler; //for rotating spaceship
+    private bool moveForward;
+    private bool moveBack;
+
+    private float counter;
+    private sitInSpaceship sittingScript; // function if is sitting
+
+    public void PlaySound(GameObject sound)
+    {
+        sound.GetComponent<AudioSource>().Play();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("safe"))
+        {
+            hitSafeZone = true;
+        }
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("safe"))
+        {
+            hitSafeZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        hitSafeZone = false;
+    }
+
+    private IEnumerator RotationMechanisms()
+    {
+        Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        euler.y += secondaryAxis.x;
+        euler.x -= secondaryAxis.y;
+        spaceship.transform.rotation = Quaternion.Euler(euler);
+        yield return null;
+    }
+    private IEnumerator MovingMechanisms()
+    {
+        //Move Forward
+        if (OVRInput.GetDown(OVRInput.Button.One) && sittingScript.isInsideSpaceship && !moveForward && !moveBack)
+        {
+            moveForward = true;
+        }
+        //Move Backward
+        else if (OVRInput.GetDown(OVRInput.Button.Two) && sittingScript.isInsideSpaceship && !moveBack && !moveForward)
+        {
+            moveBack = true;
+        }
+
+        //Stop
+        else if (OVRInput.GetUp(OVRInput.Button.Two) && sittingScript.isInsideSpaceship && moveForward && !moveBack)
+        {
+            moveForward = false;
+            moveBack = false;
+        }
+        else if (OVRInput.GetUp(OVRInput.Button.One) && sittingScript.isInsideSpaceship && moveBack && !moveForward)
+        {
+            moveBack = false;
+            moveBack = false;
+        }
+        yield return null;
+
+    }
+    public static IEnumerator Frames(int frameCount)
+    {
+        while (frameCount > 0)
+        {
+            frameCount--;
+            yield return null;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        sittingScript = SittingScriptObject.GetComponent<sitInSpaceship>();
+        rotationSpeed = defTurnSpeed; 
+        moveForward = moveBack = false;
+        //GetComponent<CharacterController>().enabled = false;//disables character controller on start (roatational problems) 
+        euler = spaceship.transform.rotation.eulerAngles;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        //Moving forward mechanism
+        StartCoroutine(MovingMechanisms());
+        StartCoroutine(RotationMechanisms());
+        if (moveForward)
+        {
+            speed = defSpeed;
+            Vector3 forward = spaceship.TransformDirection(Vector3.forward); // getting forward direction
+            spaceship.GetComponent<CharacterController>().Move(forward * speed);
+		}
+        
+        //Moving backward
+        else if (moveBack)
+        {
+            speed = reverseSpeed;
+            Vector3 forward = spaceship.TransformDirection(Vector3.forward); // getting forward direction
+            spaceship.GetComponent<CharacterController>().Move(-forward * speed);
+        }
+        else if (!moveForward && !moveBack)
+        {
+            speed = 0;
+            Vector3 forward = spaceship.TransformDirection(Vector3.forward); // getting forward direction
+            spaceship.GetComponent<CharacterController>().Move(-forward * speed);
+        }
+
+        /*
+        //Exiting out of spaceship
+        //While app button is being pressed, check if it has been held for holdTime seconds
+        if (GvrController.AppButton && sittingScript.isInsideSpaceship && hasBeenDelayed && hitSafeZone) //add hitsafezone
+        {
+            counter += Time.deltaTime;
+            if (counter > .25f)
+            {
+                moveBack = false;
+                moveForward = false;
+            }
+            if (counter > holdTime)
+            {      
+                //unparent player from spaceship
+                player.transform.parent = null;
+                
+                //rotate spaceship to original position
+                Vector3 to = new Vector3(0, 0, 0);
+                transform.eulerAngles = to; //resets the rotation of spaceship
+                player.transform.eulerAngles = to; //resets the rotation of player
+
+                //move the player
+                float xpositionPlayer = player.transform.position.x;
+                float ypositionPlayer = player.transform.position.y;
+                float zpositionPlayer = player.transform.position.z;
+                player.transform.position = new Vector3(xpositionPlayer-5, ypositionPlayer, zpositionPlayer-5);
+                sittingScript.isInsideSpaceship = false;
+
+                //play sound
+                PlaySound(exitSound);
+
+                //enable spaceshipopen gameobject
+                spaceshipOpenSound.SetActive(true);
+
+                hasBeenDelayed = false;
+            }
+        }
+        */
+
+    }
+}
--- MonsterTale/Assets/Scripts/spaceship/sitInSpaceship.cs
+++ MonsterTale/Assets/Scripts/spaceship/sitInSpaceship.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class sitInSpaceship : MonoBehaviour {
     public GameObject player;
+    public GameObject SoundOpen;
 	public bool isInsideSpaceship;
     public GameObject spaceship;
-    public GameObject spaceshipOpenSound;
     public GameObject button;
     private ButtonInteract buttonScript;
 
@@ -14,34 +14,58 @@ public class sitInSpaceship : MonoBehaviour {
         float ypositionPlayerSpaceship = spaceship.transform.position.y;
         float zpositionPlayerSpaceship = spaceship.transform.position.z;
         Vector3 to = new Vector3(0, 0, 0);
-        player.transform.localPosition = new Vector3(xpositionSpaceship, ypositionPlayerSpaceship, zpositionPlayerSpaceship); //teleports player to spaceship location
+        //player.transform.localPosition = new Vector3(xpositionSpaceship, ypositionPlayerSpaceship, zpositionPlayerSpaceship); //teleports player to spaceship location
 		isInsideSpaceship = true;
         player.transform.SetParent(spaceship.transform); //attaches player to spaceship
+        player.transform.localPosition = Vector3.zero;
         player.transform.eulerAngles = to; //rotates the player to face forward
-        //ADDED THIS NEED TO RESET CHARACTER POS
+
+    }
+    //other things that need to be done as player teleports to spaceship
+    private void Utilities()
+    {
+        //disables player character controller to stop spaceship from rotating
         player.GetComponent<CharacterController>().enabled = false;
+        player.GetComponent<OVRPlayerController>().enabled = false;
+
         //activate character controller for spaceship
         spaceship.GetComponent<CharacterController>().enabled = true;
         //disable spaceship open sound
         //DisableSound(spaceshipOpenSound);
     }
-
-    public void DisableSound(GameObject temp)
+    private void PlaySound(GameObject sound)
     {
-        temp.SetActive(false);
+        sound.GetComponent<AudioSource>().Play();
+    }
+
+    public void DisableSound(GameObject soundObject)
+    {
+        soundObject.SetActive(false);
     }
     // Use this for initialization
     void Start () {
 		isInsideSpaceship = false;
         buttonScript = button.GetComponent<ButtonInteract>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        
         if (buttonScript.on)
+        {
             TeleportToSpaceship();
+            Utilities(); //other things that need to be done as player teleports to spaceship
+            PlaySound(SoundOpen);
             buttonScript.on = false;
-    	}
+        }
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            //Debug.Log("Pressed");
+        }
+
+
+    }
 
 }
 
